87b22fe8d438de002d9a6226a5013308a6d76ce4
Excalibur1224 <bafalla@mail.csuchico.edu>
2019-09-25 16:13:44 -0700

    Added phrases based of first character of first name and last character of last name

--- main.cpp
+++ main.cpp
@@ -17,8 +17,20 @@ int main()
 
   lucky = FirstName.length();
 
-  cout<<"Welcome, "<<FirstName.at(0)<<"."<<LastName.at(0)<<"."<<", here is your fortune."<<endl;
+  cout<<"Welcome, "<<FirstName.at(0)<<"."<<LastName.at(0)<<"."<<", here is your fortune..."<<endl;
   cout<<"your lucky number is "<<lucky<<endl;
 
+  if(FirstName.at(0)=='a'||FirstName.at(0)=='A'||FirstName.at(0)=='e'||FirstName.at(0)=='E'||FirstName.at(0)=='i'||FirstName.at(0)=='I'||FirstName.at(0)=='o'||FirstName.at(0)=='O'||FirstName.at(0)=='u'||FirstName.at(0)=='U')
+  {
+  cout<<"you are destined to be famous!"<<endl;
+  }
+
+  if(LastName.back()=='a'||LastName.back()=='e'||LastName.back()=='i'||LastName.back()=='o'||LastName.back()=='u'||LastName.back()=='A'||LastName.back()=='I'||LastName.back()=='O'||LastName.back()=='U')
+{
+  cout<<"you have already met your true love."<<endl;
+
+}
+
+  cout<<"have a good day!"<<endl;
   return 0;
 }
